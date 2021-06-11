61da7506b77fb9ae74a1bcdce44e0207fc0ed51e
muharisharis13 <muharisharis13@gmail.com>
2021-06-11 14:21:56 +0700

    delete router

--- routes/routes.js
+++ routes/routes.js
@@ -12,26 +12,7 @@ router.use('/couriers', couriers_route);
 router.use('/purchasing', purchasing_route);
 router.use('/carts', cart_route);
 router.use('/login_register', login_register);
-router.get('/view_ktp/:namaImage', function (req, res) {
-    const { namaImage } = req.params
 
-    fs.readFile(`./uploads/ktp/${namaImage}`, (err, data) => {
-        res.writeHead(200, {
-            'Content-type': 'image/jpeg'
-        })
-        res.end(data)
-    })
-})
-router.get('/view_photo/:namaImage', function (req, res) {
-    const { namaImage } = req.params
-
-    fs.readFile(`./uploads/photo/${namaImage}`, (err, data) => {
-        res.writeHead(200, {
-            'Content-type': 'image/jpeg'
-        })
-        res.end(data)
-    })
-})
 router.get('/', function(req, res){
     res.write('<h1>Hello world</h1>')
 })
