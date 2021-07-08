c85e684152a08d4a7b2ce355b909484f6a1bb8e3
muharisharis13 <muharisharis13@gmail.com>
2021-07-08 10:06:16 +0700

    update if

--- routes/MobileApp/login_register/login_register.js
+++ routes/MobileApp/login_register/login_register.js
@@ -30,17 +30,28 @@ router.get('/outlet/near', async function (req, res) {
 
   try {
 
-    let data
-    await merchantModel.find().then(hasil => data = hasil.filter(hasil => parseFloat(getDistanceFromLatLonInKm({ lat1: latitude, lon1: longitude, lat2: hasil.cart_detail.lat, lon2: hasil.cart_detail.long })).toFixed(1) < 5)
-    )
+
+    if (latitude && longitude) {
+
+      let data
+      await merchantModel.find().then(hasil => data = hasil.filter(hasil => parseFloat(getDistanceFromLatLonInKm({ lat1: latitude, lon1: longitude, lat2: hasil.cart_detail.lat, lon2: hasil.cart_detail.long })).toFixed(1) < 5)
+      )
 
 
 
 
-    res.json({
-      massege: 'success',
-      data: data
-    })
+      res.json({
+        massege: 'success',
+        data: data
+      })
+    }
+    else {
+      res.json({
+        message: 'error',
+        data: 'error data'
+      })
+    }
+
 
 
 
